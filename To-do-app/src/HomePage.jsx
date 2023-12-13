19f298133b91b2a68d943243e84b6d7edfc41eec
mcomptier <labassecourtmusic@gmail.com>
2023-12-13 17:36:30 +0100

    creation of differents pages and path to them

--- To-do-app/src/HomePage.jsx
+++ To-do-app/src/HomePage.jsx
@@ -4,11 +4,20 @@ import Footer from './components/Footer.jsx'
 import Sidebar from './components/Sidebar.jsx'
 import tasks from"./assets/tasks.json"
 import TaskList from "./components/TaskList.jsx";
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <div className="pages">
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/task/:taskId" element={<TaskDetail/>} />
+          <Route path="/create-task" element={<Create/>} />
+        </Routes>
+      </div>
+
       <TaskList tasks = {tasks}/> 
       <Sidebar/>
       <Footer/>
