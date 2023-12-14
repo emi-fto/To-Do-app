4da4899086b342108ee9131c372f724f28661b2a
Emiliano Foti <foti.emiliano@gmail.com>
2023-12-14 16:01:57 +0100

    renamed app instead homepage and homepage instead of taskpage

--- To-do-app/src/App.jsx
+++ To-do-app/src/App.jsx
@@ -1,11 +1,11 @@
-import './styles/HomePage.css'
+import './styles/App.css'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 import Sidebar from './components/Sidebar.jsx'
 import tasks from"./assets/tasks.json"
 import TaskList from "./components/TaskList.jsx";
 import {Route, Routes} from 'react-router-dom'
-import TaskPage from "./Pages/TaskPage.jsx";
+import HomePage from "./Pages/HomePage.jsx";
 import TaskDetail from "./Pages/TaskDetail.jsx";
 import Create from "./Pages/Create.jsx";
 import About from "./Pages/About.jsx";
@@ -19,7 +19,7 @@ function App() {
       <Navbar/>
       <div className="pages">
         <Routes>
-          <Route path="/" element={<TaskPage/>} />
+          <Route path="/" element={<HomePage/>} />
           <Route path="/task/:taskId" element={<TaskDetail/>} />
           <Route path="/create-task" element={<Create/>} />
           <Route path="/about" element={<About/>} />
