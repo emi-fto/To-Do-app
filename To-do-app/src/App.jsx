db263dd8714b1d8dd3c4cb4881aae64fe0549d8a
mcomptier <labassecourtmusic@gmail.com>
2023-12-14 18:37:45 +0100

    creation form, uuid implement, submit don't add task yet

--- To-do-app/src/App.jsx
+++ To-do-app/src/App.jsx
@@ -13,6 +13,7 @@ import Error from "./Pages/Error.jsx";
 
 
 function App() {
+  
   return (
     <div className="App">
       <Navbar/>
--- To-do-app/src/Pages/Create.jsx
+++ To-do-app/src/Pages/Create.jsx
@@ -1,13 +1,49 @@
 import { Link } from "react-router-dom";
+import { useState } from 'react'
+import {v4 as uuidv4} from 'uuid'
+// {  "id": 1, "task": "Read the project brief", "completed": true},
 
-const Create = () => {
+
+const Create = (setTasks) => {
+    const [id, setId] = useState();
+    const [task, setTask] = useState();
+    const [completed, setCompleted] = useState(false);
+
+
+    const handleSubmit = event =>{
+        event.preventDefault()
+        console.log('Submit', {task, completed})
+        setStudents((prevTasks)=> {
+            return [ ...prevTasks, {
+                id: uuidv4(),
+                task: task,
+                completed:completed,
+            }
+            
+            
+            ]
+
+
+        }
+
+)
+    }
+    
     return ( 
-        <div>
-            Hello
+        <>
+        <h1> New Task </h1>
+        <label> Task
+        <input name="task" type="text" value={task} onChange={({target}) => setTask(target.value)}/>
+        </label>
+        <label>
+            Statut
+            <input name="completed" type="checkbox" value={completed} onChange={({target}) => setCompleted(target.value)}/>
+          </label>
 
-        </div>
-
-
+        <form onSubmit= {handleSubmit}> 
+                <button type='submit'> Create </button>
+        </form>
+        </>
      );
 }
  
