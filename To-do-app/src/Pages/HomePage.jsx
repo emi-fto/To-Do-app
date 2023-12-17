16c1a168a9edce8703fd123921244a570e3ec8a3
Emiliano Foti <foti.emiliano@gmail.com>
2023-12-17 11:57:24 +0100

    resolved conflicts and main branch working

--- To-do-app/src/Pages/HomePage.jsx
+++ To-do-app/src/Pages/HomePage.jsx
@@ -2,14 +2,12 @@ import '../styles/HomePage.css'
 import { Link } from "react-router-dom"; // <== IMPORT and IMPORTANT
 import TaskCard from "../components/TaskCard";
 
-<<<<<<< HEAD
-function HomePage (){
-const [tasks, setTasks] = useState(taskData)
+function HomePage ({tasks}) {
 return(
     <div className='mainCtn'>
         <div className="toDos">
         {tasks &&
-            tasks.map((task) =>{
+            tasks.map((task) => {
                 return(
                     <div className="taskCard">
                         <input type="checkbox" />
@@ -24,26 +22,6 @@ return(
         Completed Tasks
         </div>
     </div>
-=======
-function HomePage ({tasks}){
-return (
-        <div class="border">
-        <h1> Your Tasks</h1>
-       
-       {tasks.map(task => (
-                    <Link to={`/task/${task.id}`} key={task.id}>
-                    <TaskCard {...task}/>
-                    </Link>
-                    
-                 ))}
-               
-                <Link to="/create-task">
-                <button className="createButton">
-                    Create new Task
-                </button>
-            </Link>
-            </div>
->>>>>>> creation
 )
 }
 
