460312200c61f939b293f2e6aa53289c83c6a5d0
mcomptier <labassecourtmusic@gmail.com>
2023-12-18 15:55:35 +0100

    filter done to completed and margin-top for boxes

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,28 +1,59 @@
-import '../styles/HomePage.css'
-import { Link } from "react-router-dom"; // <== IMPORT and IMPORTANT
-import TaskCard from "../components/TaskCard";
+import React, { useState } from 'react';
+import '../styles/HomePage.css';
+import { Link } from 'react-router-dom';
+import TaskCard from '../components/TaskCard';
 
+function HomePage({ tasks }) {
+  const [todos, setTodos] = useState(tasks.filter(task => !task.completed));
+  const [completedTasks, setCompletedTasks] = useState(
+    tasks.filter(task => task.completed)
+  );
 
-function HomePage ({tasks}) {
-return(
-    <div className='mainCtn'>
-        <div className="toDos">
-        {tasks &&
-            tasks.map((task) => {
-                return(
-                    <div className="taskCard">
-                        <input type="checkbox" onChange={event => setCompletedTasks(completedTasks.push(task))} /> {/* right logic for completed tasks? */}
-                        <Link to={`/task/${task.id}`} key={task.id}>
-                        <TaskCard {...task}/>
-                        </Link>
-                    </div>
-                    )
-            })}
-        </div>
-        <div className="done">
-        </div>
+  const handleCheckboxChange = (event, task) => {
+    const isChecked = event.target.checked;
+
+    if (isChecked) {
+      // Move task from todos to completedTasks
+      setCompletedTasks([...completedTasks, task]);
+      setTodos(todos.filter(todo => todo.id !== task.id));
+    } else {
+      // Move task from completedTasks to todos
+      setTodos([...todos, task]);
+      setCompletedTasks(completedTasks.filter(completedTask => completedTask.id !== task.id));
+    }
+  };
+
+  return (
+    <div className="mainCtn">
+      <div className="toDos">
+        {todos.map(task => (
+          <div className="taskCard" key={task.id}>
+            <input
+              type="checkbox"
+              onChange={event => handleCheckboxChange(event, task)}
+            />
+            <Link to={`/task/${task.id}`}>
+              <TaskCard {...task} />
+            </Link>
+          </div>
+        ))}
+      </div>
+      <div className="done">
+        {completedTasks.map(task => (
+          <div className="taskCard" key={task.id}>
+            <input
+              type="checkbox"
+              checked
+              onChange={event => handleCheckboxChange(event, task)}
+            />
+            <Link to={`/task/${task.id}`}>
+              <TaskCard {...task} />
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
-)
+  );
 }
 
 export default HomePage;
